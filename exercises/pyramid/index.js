f53326477dee34c6475b8b1c14ba107609431084
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-05-08 13:05:12 -0600

    Add solutions for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,59 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Option 2
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
+
+pyramid(3);
+
+// // Option 1
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+
+//     console.log(level);
+//   }
+// }
+
+// Option 1 Pseudo Code
+// 1. From 0 to n (iterate through rows)
+//     1. Create an empty string, ‘level’
+//     2. From 0 to ??? (Columns)
+//         1. IF the column should have a #
+//             1. Add a ‘#’ to ‘level’
+//         2. ELSE 
+//             1. Add a space to ‘level’
+//     3. Console log ‘stair’
+
 
 module.exports = pyramid;

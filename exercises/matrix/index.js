9b5663c09780ab8187e8c93aedd47b6e99b42105
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-05-08 14:48:22 -0600

    Add solution for matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,64 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top Row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    };
+    startRow++;
+
+    // Right column
+    for (let i = startRow; i<= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    };
+    endColumn--;
+
+    // Bottom row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    };
+    endRow--;
+
+    // Start Column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    };
+    startColumn++;
+  };
+
+  return results;
+}
+
+// Option 1 Pseudo Code
+// 1. Create empty arrays of arrays called ‘results’
+// 2. Create a counter variable, starting at 1
+// 3. As long as (start column <= end column) AND (start row <= end row)
+//     1. Loop from start column to end column
+//         1. At results[start_row][I] assign counter variable
+//         2. Increment counter
+//     2. Increment start row
+//     3. Loop from start row to end row
+//         1. At results[I][end_column] assign counter variable
+//         2. Increment counter
+//     4. Decrement end column
+//     5. …repeat for other two sides
+
 
 module.exports = matrix;

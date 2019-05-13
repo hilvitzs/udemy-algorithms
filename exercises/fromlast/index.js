d2b6b7ba796032777e1accd39960e7774968a836
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-05-13 15:52:17 -0600

    add solution to fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,21 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let first = list.head;
+  let second = list.head;
+
+  while (n > 0) {
+    second = second.next;
+    n--;
+  }
+
+  while (second.next) {
+    first = first.next;
+    second = second.next;
+  }
+
+  return first;
+}
 
 module.exports = fromLast;

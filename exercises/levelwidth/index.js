e43c66f41899607a90b34bed400ef12f9d936fcf
Spencer Hilvitz <spencer.hilvitz@wipro.com>
2019-05-14 16:12:22 -0600

    Add levelwidth solution, add tree solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root, 'stop'];
+  let counter = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 'stop') {
+      counter.push(0);
+      arr.push(node);
+    } else {
+      arr.push(...node.children);
+      counter[counter.length - 1]++;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = levelWidth;
